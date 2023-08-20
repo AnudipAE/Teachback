0c42a03049ca0f114919f2ad37699aaca84e61ce
AnudipAE <142653712+AnudipAE@users.noreply.github.com>
2023-08-20 22:33:35 +0530

    2D Array vs HashMap

--- 2D Array vs HashMap/src/operations/Menu.java
+++ 2D Array vs HashMap/src/operations/Menu.java
@@ -2,12 +2,17 @@ package operations;
 
 import java.util.Scanner;
 
+//class menu
 public class Menu {
 
+	//Scanner object for taking user input
 	Scanner sc = new Scanner(System.in);
 
+	//method for displaying menu
 	public int menu() {
 		System.out.println();
+
+		//menu displaying
 		System.out.println("1. Display Phone Book in 2D Array.");
 		System.out.println("2. Display Phone Book in Hash Map.");
 		System.out.println("3. Insert Name in 2D Array. ");
@@ -16,9 +21,10 @@ public class Menu {
 		System.out.println("6. Time taken for insertion in 2D Array.");
 		System.out.println("7. Time taken for insertion in Hash Map.");
 		System.out.println("8. Exit.");
-		int choice = sc.nextInt();
 
-		return choice;
-	}
+		int choice = sc.nextInt(); // taking the input from the user and assigning to variable choice
 
-}
+		return choice; // returning choice
+	} // end of menu method
+
+}// method of class menu
--- 2D Array vs HashMap/src/operations/Operations.java
+++ 2D Array vs HashMap/src/operations/Operations.java
@@ -8,37 +8,50 @@ import twoDArray.TwoDArrayImpl;
 
 public class Operations {
 
+	//creating scanner object
 	Scanner sc = new Scanner(System.in);
 
+	//creating menu class object
 	Menu menu = new Menu();
 
+	//creating TwoDArrayImpl class object 
 	TwoDArrayImpl t1 = new TwoDArrayImpl();
 
+	//creating HashMapImpl class object 
 	HashMapImpl h1 = new HashMapImpl();
 
+	//creating a 2D array
 	String[][] array2D;
 
+	//creating hashmap
 	Map<String, String> hashMap1;
 
+	//Method for performing operations
 	public void operations() {
-		int choice = 0;
-		int numEntries = 10;
-		array2D = t1.twoDArray(numEntries);
-		hashMap1 = h1.hashMap(numEntries);
+		int choice = 0;		// creating variable for while loop
+		int numEntries = 10;	// variable for creating phone book entries
+		array2D = t1.twoDArray(numEntries);	// calling twoDArray method and assigning the value
+		hashMap1 = h1.hashMap(numEntries);	// calling hashmap method and assigning the value
 
+		// while loop 
 		while (choice != 8) {
-			choice = menu.menu();
+			choice = menu.menu();	//calling menu method for displaying the menu
+
+			//starting switch case
 			switch (choice) {
+			//case 1 for displaying phone book in 2D array	
 			case 1: {
 				System.out.println("Phone Book in 2D Array : ");
 				t1.display2DArray(array2D);
 				break;
 			}
+			//case 2 for displaying phone book in hashMap
 			case 2: {
 				System.out.println("Phone Book in Hash Map : ");
 				h1.displayHashMap(hashMap1);
 				break;
 			}
+			//case 3 for inserting name in 2D array
 			case 3: {
 				System.out.println("Insertion in 2D Array : ");
 				System.out.println("Enter the name row : ");
@@ -51,6 +64,7 @@ public class Operations {
 				System.out.println("Insertion completed.");
 				break;
 			}
+			//case 4 for inserting number in 2D array
 			case 4: {
 				System.out.println("Insertion in 2D Array : ");
 				System.out.println("Enter the number row : ");
@@ -63,6 +77,7 @@ public class Operations {
 				System.out.println("Insertion completed.");
 				break;
 			}
+			//case 5 for inserting name and number in hashmap
 			case 5: {
 				System.out.println("Insertion in Hash Map : ");
 				System.out.println("Enter the name : ");
@@ -74,6 +89,7 @@ public class Operations {
 				System.out.println("Insertion completed.");
 				break;
 			}
+			//case 6 for calculating time taken by 2d array for inserting
 			case 6: {
 				long startTime = System.nanoTime();
 
@@ -84,6 +100,7 @@ public class Operations {
 				System.out.println("Time taken: " + elapsedTime + " nanoseconds");
 				break;
 			}
+			//case 7 for calculating time taken by hashmap for inserting
 			case 7: {
 				long startTime = System.nanoTime();
 
@@ -94,16 +111,18 @@ public class Operations {
 				System.out.println("Time taken: " + elapsedTime + " nanoseconds");
 				break;
 			}
+			//case 8 for exiting the program
 			case 8: {
 				System.out.println("Thank You.");
 				System.exit(0);
 			}
+			//default case
 			default:
 				throw new IllegalArgumentException("Unexpected value: " + choice);
-			}
-		}
+			}// end of switch case
+		}//end of while loop
 
-		sc.close();
-	}
+		sc.close(); // closing scanner object
+	}// end of method operations
 
-}
+}// end of class operations
