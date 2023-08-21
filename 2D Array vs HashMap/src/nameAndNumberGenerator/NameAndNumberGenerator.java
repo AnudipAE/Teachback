3e1247faad760d33a9ba566334c227bc61af4c1a
AnudipAE <142653712+AnudipAE@users.noreply.github.com>
2023-08-21 11:00:17 +0530

    2D Array vs HashMap

--- 2D Array vs HashMap/src/nameAndNumberGenerator/NameAndNumberGenerator.java
+++ 2D Array vs HashMap/src/nameAndNumberGenerator/NameAndNumberGenerator.java
@@ -4,18 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+//class NameAndNumberGenerator
 public class NameAndNumberGenerator {
 
+	//randam first name for phone book
 	private static final String[] FIRST_NAMES = { "Alice", "Bob", "Charlie", "David", "Emma", "Frank", 
 			"Grace", "Henry","Isabella", "Jack", "Katherine", "Liam", "Mia", "Noah", "Olivia", "Peter"
 			// ... add more first names as needed
 	};
 
+	//randam last name for phone book
 	private static final String[] LAST_NAMES = { "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller",
 			"Wilson"
 			// ... add more last names as needed
 	};
 
+	//method for assigning names in 2D array and HashMap
 	public List<String> generateFictitiousNames(int numEntries) {
 		if (numEntries <= 0) {
 			throw new IllegalArgumentException("Number of entries must be a positive integer");
@@ -31,9 +35,9 @@ public class NameAndNumberGenerator {
 		}
 
 		return generatedNames;
-	}
+	}//end of method generateFictitiousNames
 	
-	
+	//method for assigning number in 2D array and HashMap
 	public List<String> generateFictitiousPhoneNumbers(int numEntries) {
         if (numEntries <= 0) {
             throw new IllegalArgumentException("Number of entries must be a positive integer");
@@ -51,6 +55,6 @@ public class NameAndNumberGenerator {
         }
 
         return generatedPhoneNumbers;
-    }
+    }//end of method generateFictitiousPhoneNumber
 
-}
+}//end of class NameAndNumberGenerator
