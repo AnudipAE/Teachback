1ffcc5f76f10fc88567c0925ac46529c65f863d1
AnudipAE <142653712+AnudipAE@users.noreply.github.com>
2023-11-27 01:43:40 +0530

    Delete Teachback/E-commerceSystem directory

--- Teachback/E-commerceSystem/ecom-mock-testing/src/main/java/com/project/mocktest/ThirdPartyServerClient.java
+++
@@ -1,7 +0,0 @@
-package com.project.mocktest;
-
-public interface ThirdPartyServerClient {
-    ProductResponse getProductInfo(String productId);
-    OrderResponse placeOrder(OrderDetails orderDetails);
-    CustomerResponse getCustomerInfo(String customerId);
-}
